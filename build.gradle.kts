34b345c744978730027601a1f0a6f26672153358
tiennm99 <tiennm99@outlook.com>
2025-11-13 18:24:13 +0700

    feat(loki): send logs to loki to centralize logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ configurations {
 dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.36")
     implementation("com.couchbase.client:java-client:3.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.google.guava:guava:33.4.0-jre")
     implementation("org.apache.commons:commons-math3:3.6.1")
@@ -32,6 +33,7 @@ dependencies {
     implementation("org.telegram:telegrambots-client:8.0.0")
     implementation("org.telegram:telegrambots-extensions:8.0.0")
     implementation("org.telegram:telegrambots-longpolling:8.0.0")
+    implementation("pl.tkowalcz.tjahzi:log4j2-appender:0.9.32")
 
     testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
