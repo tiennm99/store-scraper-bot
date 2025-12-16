d99798fae7f6c9866724ecf9851982b74cfb43c9
tiennm99 <tiennm99@outlook.com>
2025-12-16 22:08:25 +0700

    feat(repo): cleanup

--- src/main/java/com/miti99/storescraperbot/model/Admin.java
+++ src/main/java/com/miti99/storescraperbot/model/Admin.java
@@ -3,10 +3,8 @@ package com.miti99.storescraperbot.model;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class Admin extends AbstractModel {
-  Set<Long> groups = new HashSet<>();
+  final Set<Long> groups = new HashSet<>();
 }
