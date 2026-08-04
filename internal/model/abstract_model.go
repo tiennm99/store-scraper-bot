ca1a409610cf523ceebb2ec83b6d8b6aac09d8bf
tiennm99 <tiennm99@outlook.com>
2026-08-04 23:00:01 +0700

    Clear and rewrite
    Retire the Go port; history continues from the JavaScript implementation (store-scraper-bot).

--- internal/model/abstract_model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// AbstractModel mirrors Java AbstractModel: every persisted entity has _id (string)
-// and a `class` discriminator equal to the simple type name.
-type AbstractModel struct {
-	ID    string `bson:"_id" json:"_id"`
-	Class string `bson:"class" json:"class"`
-}
